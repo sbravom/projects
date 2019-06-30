38106d7fee1ad552b30961bbd95754c5a32f78ed
sergio bravo <sergio.bravo.moreno@gmail.com>
2019-06-30 17:51:58 +0100

    Add files via upload

--- 4_holdings_by_age_over_time/AUX_get_holdings.R
+++ 4_holdings_by_age_over_time/AUX_get_holdings.R
@@ -12,7 +12,7 @@ get_holdings <- function(yyyy, mm) {
       if (yyyy<=2015) {
         source <- paste0(URL11, yyyy, "/", mm, URL12)
       } else if (yyyy==2016) {
-        if (mm %in% c("03")) {
+        if (mm %in% c("03", "06")) {
           source <- paste0(URL11, yyyy, "/", mm, URL12)
         } else {
           source <- paste0(URL21, yyyy, "/", mm, URL22)
@@ -22,17 +22,18 @@ get_holdings <- function(yyyy, mm) {
       aux <- readHTMLTable(source)[[1]]  
       names <-  aux[, 1]
       rownames(aux) <-  make.names(names, unique=TRUE)
-      subset <- c("Hasta.45", paste0("X", 46:70), "Mas.de.70")
+      subset <- c(paste0("X", 46:70))
       aux <- aux[subset, c(1, 6, 7 , 9)]
       rownames(aux) <- NULL
       names(aux) <- c("age", "avg_holdings_males", "avg_holdings_fem", "avg_holdings")
      # age              <- data.frame(age = c("Hasta 45", 46:70, "Más de 70"))
      # levels(age)      <- c("Hasta 45", c(46:70), "Más de 70")
-      aux$age <- 45:71
+     # aux$age <- 45:71
       aux <- map_df(aux, ~ str_remove_all(.x, "\\.")) %>% 
         map_df(as.numeric)  
      # aux <- bind_cols(age, aux)
-      aux$date <- as.Date(rep(paste0(yyyy, "-", mm,"-01"), each = 27))
+      aux$date <- as.Date(rep(paste0(yyyy, "-", mm,"-01"), each = nrow(aux)))
+      aux$age <- as.numeric(aux$age)
       aux
     },
     # Catch part
